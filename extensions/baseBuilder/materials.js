4162352a821caabd426a3bdfaa4fe4587ccdf28a
unknown <ceasedinsanity.ct@gmail.com>
2021-05-04 21:10:21 -0300

    Added all basic building materials

--- extensions/baseBuilder/materials.js
+++ extensions/baseBuilder/materials.js
@@ -4,5 +4,9 @@ export default [
   WORLD.TILES.steel_block,
   WORLD.TILES.steel_door,
   WORLD.TILES.scrap_block,
-  WORLD.TILES.scrap_door
+  WORLD.TILES.scrap_door,
+  WORLD.TILES.sign_block,
+  WORLD.TILES.small_chest,
+  WORLD.TILES.large_chest,
+  WORLD.TILES.anchor
 ];
