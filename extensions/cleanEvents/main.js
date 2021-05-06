c2d451963d77abbf92ec8382788e83f3ad5898ce
unknown <ceasedinsanity.ct@gmail.com>
2021-05-06 02:44:51 -0300

    Feature (CleanEvents): Added some phrases to remove from logs

--- extensions/cleanEvents/main.js
+++ extensions/cleanEvents/main.js
@@ -8,6 +8,23 @@ const extension = new Extension({
 
 let checkMoveLog = null;
 let applyServerEvent = null;
+let engineLog = null;
+
+const removedPhrases = [
+  "you earned", // XP
+  "leveled up",
+  "finally summer ends", // Seasons
+  "the hot air rests",
+  "the cool air rests still over",
+  "the warm air rests",
+  "winter passes",
+  "another year has passed",
+  "the air grows cooler",
+  "the air becomes almost unbearably",
+  "the warm air rests still over the",
+  "maybe there's some", // Events
+  "body overburdened"
+]
 
 extension.onStart(() => {
   // Remove move logs
@@ -17,6 +34,21 @@ extension.onStart(() => {
   // Remove event logs
   applyServerEvent = EVENTS.applyServerEvent;
   EVENTS.applyServerEvent = eval("(" + applyServerEvent.toString().replace("ENGINE.log(desc);", "") + ")");
+
+  // Don't print some server events
+  engineLog = ENGINE.log;
+  ENGINE.log = (text, replaceOldSame) => {
+    
+    // Dont log these
+    let isRemoved = false;
+    removedPhrases.forEach((phrase) => {
+      if (text.contains(phrase)) isRemoved = true;
+    });
+
+    if (isRemoved) return;
+
+    engineLog(text, replaceOldSame);
+  };
 });
 
 extension.onStop(() => {
@@ -25,6 +57,7 @@ extension.onStop(() => {
 
   // Restore event logs
   EVENTS.applyServerEvent = applyServerEvent;
+  ENGINE.log = engineLog;
 });
 
 export default extension;
